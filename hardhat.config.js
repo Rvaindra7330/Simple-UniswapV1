c7935d9a0d746a4c909f1d9f808e6fc10bc16d20
Ravindra7330 <ravindarravi87@gmail.com>
2025-08-25 11:01:05 +0530

    sepolia RPC config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
+const { INFURA_API_KEY, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks:{
+      sepolia: {
+      url: `${INFURA_API_KEY}` ,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
