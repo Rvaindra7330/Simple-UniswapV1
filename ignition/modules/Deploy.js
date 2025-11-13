f6a3c0694453ce9677be7e1e4789c0ee7ef1d605
Ravindra7330 <ravindarravi87@gmail.com>
2025-11-13 13:46:31 +0530

    tested locally

--- ignition/modules/Deploy.js
+++ ignition/modules/Deploy.js
@@ -3,24 +3,24 @@ const hre = require("hardhat");
 async function main() {
   const TokenA = await hre.ethers.getContractFactory("TokenA");
   const tokenA = await TokenA.deploy();
-  await tokenA.deployed();
-  console.log("TokenA:", tokenA.address);
+  await tokenA.waitForDeployment();
+  console.log("TokenA:", tokenA.target);
 
   const TokenB = await hre.ethers.getContractFactory("TokenB");
   const tokenB = await TokenB.deploy();
-  await tokenB.deployed();
-  console.log("TokenB:", tokenB.address);
+  await tokenB.waitForDeployment();
+  console.log("TokenB:", tokenB.target);
 
   const Factory = await hre.ethers.getContractFactory("Factory");
   const factory = await Factory.deploy();
-  await factory.deployed();
-  console.log("Factory:", factory.address);
+  await factory.waitForDeployment();
+  console.log("Factory:", factory.target);
 
   // Create a Pair
-  const tx = await factory.createPair(tokenA.address, tokenB.address);
+  const tx = await factory.createPair(tokenA.target, tokenB.target);
   await tx.wait();
 
-  const pairAddress = await factory.getPair(tokenA.address, tokenB.address);
+  const pairAddress = await factory.getPair(tokenA.target, tokenB.target);
   console.log("Pair Address:", pairAddress);
 }
 
